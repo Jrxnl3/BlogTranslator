e15541dd576297f90b4e2f0c8cd5aec70287320f
Jan Büchner <jan.buechner04@gmail.com>
2022-09-11 01:13:11 +0200

    Downloading Text + to HTML Working, other way not :/

--- src/test/java/de/jinx/blogtranslator/MainPageTest.java
+++ src/test/java/de/jinx/blogtranslator/MainPageTest.java
@@ -7,6 +7,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -14,8 +15,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLOutput;
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,8 +28,8 @@ import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-public class MainPageTest {
-    MainPage mainPage = new MainPage();
+public class MainPageTest implements Runnable {
+    List<String> translationTexts;
 
     @BeforeAll
     public static void setUpAll() {
@@ -34,37 +39,82 @@ public class MainPageTest {
 
     @BeforeEach
     public void setUp() {
-        open("https://www.deepl.com/translator#de/en/");
+        open("https://smartblogger.com/make-money-blogging/");
     }
 
-    /*
-    public void translatedText() {
+    public void loadSite() {
         try {
-            WebDriverWait wait = new WebDriverWait(WebDriverRunner.getWebDriver(), Duration.ofSeconds(5));
+            WebDriverWait wait = new WebDriverWait(WebDriverRunner.getWebDriver(), Duration.ofSeconds(1));
             wait.until(ExpectedConditions.visibilityOf($x("//div[@class='lmt__translations_as_text__text_btn']")));
         }
-        catch (Exception e) {
-            System.out.println("Element not found");
+        catch (Exception ignored) {}
+    }
+
+    public void startTranslation() throws IOException {
+        loadSite();
+
+        File file = new File("./blog_translated.txt");
+        FileWriter fileWriter = new FileWriter(file);
+
+        for (String text : translationTexts) {
+            open("https://www.deepl.com/translator#en/de/"+text);
+            loadSite();
+
+            Document doc = Jsoup.parse(WebDriverRunner.getWebDriver().getPageSource());
+            fileWriter.write(doc.getElementById("target-dummydiv").text() + "\n");
         }
 
-        Document doc = Jsoup.parse(WebDriverRunner.getWebDriver().getPageSource());
-
-        System.out.println("Translated: " + doc.getElementById("target-dummydiv").text());
     }
-*/
 
     @Test
-    public void translate(){
+    public void getAllText() throws IOException {
+        loadSite();
 
+        Document doc = Jsoup.parse(WebDriverRunner.getWebDriver().getPageSource());
+        Element parentTextHolder = doc.getElementsByClass("entry-content clear").first();
+
+        File file = new File("./blog.html");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("<body>\n");
+
+        for (int i = 0; i < parentTextHolder.children().size(); i++) {
+            Element child = parentTextHolder.child(i);
+            //if tag a paragh or header then continue
+            if (child.tagName().equals("a") || child.tagName().equals("p") || child.tagName().equals("h1") || child.tagName().equals("h2") || child.tagName().equals("h3") || child.tagName().equals("h4") || child.tagName().equals("h5") || child.tagName().equals("h6")) {
+                String formattedString = String.format("\t<%s>%s</%s> \n", child.tag(), child.text(), child.tag());
+
+                fileWriter.write(formattedString);
+            }
+        }
+        loadSite();
+        fileWriter.write("</body>");
+        fileWriter.close();
+
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            startTranslation();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+/*
+    @Test
+    public void translate() throws IOException {
         try {
             WebDriverWait wait = new WebDriverWait(WebDriverRunner.getWebDriver(), Duration.ofSeconds(1));
             wait.until(ExpectedConditions.visibilityOf($x("//div[@class='lmt__translations_as_text__text_btn']")));
         }
         catch (Exception ignored) {}
 
+        copyBlogText();
 
-        mainPage.toTranslate.add("Hello how is it going");
-        mainPage.toTranslate.add("What are you doing?");
+        File file = new File("./blog_translated.txt");
+        FileWriter fileWriter = new FileWriter(file);
 
         for(String satz : mainPage.toTranslate){
             //Enter text
@@ -80,10 +130,26 @@ public class MainPageTest {
 
             //Get translation
             Document doc = Jsoup.parse(WebDriverRunner.getWebDriver().getPageSource());
-            System.out.println("Translated: " + doc.getElementById("target-dummydiv").text());
+
+            fileWriter.write(doc.getElementById("target-dummydiv").text());
         }
     }
+*/
     /*
+    public void translatedText() {
+        try {
+            WebDriverWait wait = new WebDriverWait(WebDriverRunner.getWebDriver(), Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOf($x("//div[@class='lmt__translations_as_text__text_btn']")));
+        }
+        catch (Exception e) {
+            System.out.println("Element not found");
+        }
+
+        Document doc = Jsoup.parse(WebDriverRunner.getWebDriver().getPageSource());
+
+        System.out.println("Translated: " + doc.getElementById("target-dummydiv").text());
+    }
+
     @Test
     public void search() {
         mainPage.searchButton.click();
---
+++ src/test/java/de/jinx/blogtranslator/ThreadManager.java
@@ -0,0 +1,45 @@
+package de.jinx.blogtranslator;
+
+import com.lowagie.text.DocumentException;
+import com.sun.tools.javac.Main;
+import org.fit.pdfdom.PDFToHTML;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ThreadManager {
+    public static void main(String[] args) {
+        MainPage mainPage = new MainPage();
+        try {
+            mainPage.createTranslationText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        generatePDF("./blog.html", "./blog_toTranslate.pdf");
+        PDFToHTML.main(new String[]{"./blog_toTranslate.pdf", "./blog_translated.html"});
+    }
+    public static void generatePDF(String inputHtmlPath, String outputPdfPath)
+    {
+        try {
+            String url = new File(inputHtmlPath).toURI().toURL().toString();
+            System.out.println("URL: " + url);
+
+            OutputStream out = new FileOutputStream(outputPdfPath);
+
+            //Flying Saucer part
+            ITextRenderer renderer = new ITextRenderer();
+
+            renderer.setDocument(url);
+            renderer.layout();
+            renderer.createPDF(out);
+
+            out.close();
+        } catch (DocumentException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
